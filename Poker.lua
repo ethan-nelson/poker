c19d89dd9087d9b60faf7d6e84e2545c81cee640
ethan-nelson <ethan-nelson@users.noreply.github.com>
2014-10-15 16:35:14 -0500

    adding face cards

--- Poker.lua
+++ Poker.lua
@@ -3,18 +3,18 @@ pokerinfo = GetAddOnMetadata("Poker", "Version");
 function Poker_OnLoad(self)
 
 	SLASH_POKER1, SLASH_POKER2 = "/poker", "/Poker";
-	DEFAULT_CHAT_FRAME:AddMessage("{Poker} raid addon has loaded; you must be in a raid to use this addon!", 1, 1, 1);
+	DEFAULT_CHAT_FRAME:AddMessage("Poker raid addon has loaded; you must be in a raid to use this addon!", 1, 1, 1);
 	
 end
 
 
 function Poker_Print(statement)
-	DEFAULT_CHAT_FRAME:AddMessage("{Poker}: " .. statement, 0.5, 0.5, 1);
+	DEFAULT_CHAT_FRAME:AddMessage("{P}: " .. statement, 0.5, 0.5, 1);
 end
 
 
 function Poker_Tell_Raid(statement)
-	SendChatMessage("{Poker}: " .. statement,"RAID",nil,nil);
+	SendChatMessage("{P}: " .. statement,"RAID",nil,nil);
 end
 
 
@@ -22,7 +22,7 @@ function SlashCmdList.POKER(self)
 	rolls = {};
 	cards = {};
 	Poker_Rolls();
-	Poker_Tell_Raid("Incoming hand: ");
+	Poker_Tell_Raid("Incoming poker hand: ");
 	Poker_Determine_Suit();
 end
 
@@ -48,13 +48,53 @@ function Poker_Determine_Suit()
 	
 	for i = 1, 5 do
 		if (rolls[i] >= 1 and rolls[i] <= 13) then
-			cards[i] = tostring(rolls[i]) .. " of Spades.";
+			if (rolls[i] == 1) then
+				cards[i] = "Ace of  Clubs.";
+			elseif (rolls[i] == 11) then
+				cards[i] = "Jack of Clubs.";
+			elseif (rolls[i] == 12) then
+				cards[i] = "Queen of Clubs.";
+			elseif (rolls[i] == 13) then
+				cards[i] = "King of Clubs.";
+			else
+				cards[i] = tostring(rolls[i]) .. " of Clubs.";
+			end
 		elseif (rolls[i] >= 14 and rolls[i] <= 26) then
-			cards[i] = tostring(rolls[i]-13) .. " of Clubs.";
+			if (rolls[i] == 1+13) then
+				cards[i] = "Ace of Diamonds.";
+			elseif (rolls[i] == 11+13) then
+				cards[i] = "Jack of Diamonds.";
+			elseif (rolls[i] == 12+13) then
+				cards[i] = "Queen of Diamonds.";
+			elseif (rolls[i] == 13+13) then
+				cards[i] = "King of Diamonds.";
+			else
+				cards[i] = tostring(rolls[i]-13) .. " of Diamonds.";
+			end
 		elseif (rolls[i] >= 27 and rolls[i] <= 39) then
-			cards[i] = tostring(rolls[i]-26) .. " of Hearts.";
+			if (rolls[i] == 1+26) then
+				cards[i] = "Ace of Hearts.";
+			elseif (rolls[i] == 11+26) then
+				cards[i] = "Jack of Hearts.";
+			elseif (rolls[i] == 12+26) then
+				cards[i] = "Queen of Hearts.";
+			elseif (rolls[i] == 13+26) then
+				cards[i] = "King of Hearts.";
+			else
+				cards[i] = tostring(rolls[i]-26) .. " of Hearts.";
+			end
 		elseif (rolls[i] >= 40 and rolls[i] <= 52) then
-			cards[i] = tostring(rolls[i]-39) .. " of Diamonds.";
+			if (rolls[i] == 1+39) then
+				cards[i] = "Ace of Spades.";
+			elseif (rolls[i] == 11+39) then
+				cards[i] = "Jack of Spades.";
+			elseif (rolls[i] == 12+39) then
+				cards[i] = "Queen of Spades.";
+			elseif (rolls[i] == 13+39) then
+				cards[i] = "King of Spades.";
+			else
+				cards[i] = tostring(rolls[i]-39) .. "of Spades.";
+			end
 		else
 			Poker_Print("ERROR IN POKER PROGRAM. PLEASE SUBMIT TO bugs@forthewynn.info THAT YOU ROLLED A " .. rolls[i]);
 			Poker_Print("ALSO MENTION YOU ARE USING VERSION " .. pokerinfo);
